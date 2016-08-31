7b39206b5e89f8e42dd3ba9291da9e5bf3e869a1
3b295 <xwystz@gmail.com>
2016-08-31 14:51:16 +0000

    修正测试文件

--- monitor/text.py
+++ monitor/text.py
@@ -4,11 +4,14 @@ import time
 
 cap = cv2.VideoCapture(0)
 
-if cap.isOpened:
+while cap.isOpened:
     now_time = time.time()
     now_time_str = time.strftime('%Y-%m-%d %H %M %S', time.localtime(now_time))
-    print('OK')
     ret, frame = cap.read()
+    print('ok')
     if ret:
         cv2.imwrite('img/' + now_time_str + '.jpg', frame)
         print('writed')
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
